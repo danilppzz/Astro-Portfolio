9d9eda51f051a4a28648936f809fc6b00d90e0dd
d4niz <josedanielmorebolopez@gmail.com>
2024-03-04 11:37:40 +0100

    Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://danilppzz.dev",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "es", "de"]
+  },
+  pages: [{
+    src: '/:lang/*',
+    data: async ({
+      params
+    }) => {
+      const lang = params.lang || 'en';
+      return {
+        lang
+      };
+    }
+  }],
+  integrations: [tailwind()]
+});
